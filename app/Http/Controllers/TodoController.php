45078d15a2198cd3c1b7d7c85f35da491296a6ac
fathimafida <fidafirozkattungal@gmail.com>
2024-08-29 20:27:49 +0530

    done

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -46,7 +46,14 @@ class TodoController extends Controller
     {
         return view('todos.edit',compact('todo'));
     }
-
+    // Here we use the Request object to validate the data sent from the form
+    // The Request object is a facade provided by Laravel that allows us to interact with the current HTTP request
+    // We use the validate method to check if the data sent from the form is valid according to the rules we define
+    // In this case we check if the title and description are required and have a minimum length of 3 characters
+    // If the validation fails, the user will be redirected back to the form with the errors
+    // If the validation passes, we update the todo item with the new data
+    // We use the update method provided by the Eloquent ORM to update the todo item
+    // Finally we redirect the user back to the todo list page with a success message
     public function update(Request $request, Todo $todo)
     {
         $request->validate([
